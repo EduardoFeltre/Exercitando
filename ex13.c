4c3e152f7dcdc37daf345019bc69867eff72c6d5
Eduardo Vieira Feltre <dudufeltre@gmail.com>
2025-02-22 15:23:41 -0300

    att

---
+++ ex13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, x, Y;
+
+    printf("Digite o valor da razao da PA:\n");
+    scanf("%d", &a);
+    printf("Digite o numero do termo:\n");
+    scanf("%d", &x);
+
+    Y = a*x;
+
+    printf("%d", Y);
+
+    return 0;
+}
