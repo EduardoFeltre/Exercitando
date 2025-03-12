b62b0270f9856861c2e3cde0211197996149c5e3
Eduardo Vieira Feltre <dudufeltre@gmail.com>
2025-03-12 15:10:02 -0300

    att

--- ex20.c
+++ ex20.c
@@ -4,10 +4,10 @@ int main() {
     int n, i = 0, num = 1;
     
     
-    printf("Digite a quantidade de termos da sequência: ");
+    printf("Digite a quantidade de termos da sequencia: ");
     scanf("%d", &n);
     
-    printf("Sequência: ");
+    printf("Sequencia: ");
     
     while (i < n) {
         printf("%d ", num);
