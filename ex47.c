5b019cafdde3c1036ee4ea2aa7e4d3d09966ebf5
Eduardo Vieira Feltre <dudufeltre@gmail.com>
2025-04-28 07:35:15 -0300

    att

--- ex47.c
+++ ex47.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Função que troca os valores de dois inteiros
+
 void troca(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -10,19 +10,19 @@ void troca(int *a, int *b) {
 int main() {
     int num1, num2;
 
-    // Lê dois números do usuário
+    
     printf("Digite o primeiro numero: ");
     scanf("%d", &num1);
     printf("Digite o segundo numero: ");
     scanf("%d", &num2);
 
-    // Mostra os valores antes da troca
+    
     printf("Antes da troca: num1 = %d, num2 = %d\n", num1, num2);
 
-    // Chama a função para trocar os valores
+    
     troca(&num1, &num2);
 
-    // Mostra os valores após a troca
+    
     printf("Depois da troca: num1 = %d, num2 = %d\n", num1, num2);
 
     return 0;
