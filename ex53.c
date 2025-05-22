2632f278a4d039c5d6ebb96cef5a2646e9fdc15f
Eduardo Vieira Feltre <dudufeltre@gmail.com>
2025-05-22 16:28:20 -0300

    att

--- ex53.c
+++ ex53.c
@@ -9,11 +9,11 @@ int main() {
     printf("Quantos numeros aleatorios deseja gerar? ");
     scanf("%d", &n);
 
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
+    srand(time(NULL)); 
 
     printf("Numeros aleatorios entre 1 e 15:\n");
     for (i = 0; i < n; i++) {
-        randomNumber = (rand() % 15) + 1; // Gera números entre 1 e 15
+        randomNumber = (rand() % 15) + 1; 
         printf("%d ", randomNumber);
     }
 
